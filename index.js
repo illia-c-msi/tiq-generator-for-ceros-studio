982ceb92b4a37b3e659f8c1e8b23ef21b525f351
illia-c-msi <illia.chykurov@motorolasolutions.com>
2025-02-17 14:59:38 +0100

    add: initial logic

---
+++ index.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        const localeOptions = {
+            EMEA: [
+                { value: 'en_xu', label: 'en_xu' },
+                { value: 'de_xc', label: 'de_xc' },
+                { value: 'ru_ru', label: 'ru_ru' },
+                { value: 'fr_fr', label: 'fr_fr' },
+                { value: 'pl_pl', label: 'pl_pl' },
+                { value: 'he_il', label: 'he_il' },
+            ],
+            NAMR: [
+                { value: 'en_us', label: 'en_us' },
+                { value: 'en_ca', label: 'en_ca' },
+            ],
+            APAC: [
+                { value: 'en_xa', label: 'en_xa' },
+                { value: 'ja_jp', label: 'ja_jp' },
+                { value: 'ko_kr', label: 'ko_kr' },
+                { value: 'zh_tw', label: 'zh_tw' },
+                { value: 'en_xp', label: 'en_xp' },
+                { value: 'zh_cn', label: 'zh_cn' },
+            ],
+            LACR: [
+                { value: 'en_xl', label: 'en_xl' },
+                { value: 'es_xl', label: 'es_xl' },
+                { value: 'pt_xl', label: 'pt_xl' },
+            ],
+        };
+        $('#region').change(function () {
+            const selectedRegion = $(this).val();
+            $('#locale').empty();
+            $('#locale').prop('disabled', true);
+
+            if (selectedRegion) {
+                const options = localeOptions[selectedRegion];
+                if (options) {
+                    options.forEach((option) => {
+                        $('#locale').append(
+                            $('<option>', {
+                                value: option.value,
+                                text: option.label,
+                            }),
+                        );
+                    });
+                    $('#locale').prop('disabled', false);
+                }
+            }
+        });
+
+        $('#tealium-form').submit(function (e) {
+            e.preventDefault();
+
+            const region = $('#region').val();
+            const locale = $('#locale').val();
+            const pageType = $('#page-type').val();
+
+            if (!region || !locale || !pageType) {
+                alert('Please select Region, Locale, and Page Type.');
+                return;
+            }
+
+            const generateCode = (env) => {
+                const utagUrl = `https://tags.tiqcdn.com/utag/motorola/campaign/${env}/utag.sync.js`;
+                const utagData = `var utag_data = {
+	msi_platform: 'ceros',
+	region: '${region.toLowerCase()}',
+	locale: '${locale}',
+	page_type: '${pageType}',
+};`;
+                const utagScript = `(function (a, b, c, d) {
+  a = 'https://tags.tiqcdn.com/utag/motorola/campaign/${env}/utag.js';
+  b = document;
+  c = 'script';
+  d = b.createElement(c);
+  d.src = a;
+  d.type = 'text/java' + c;
+  d.async = true;
+  a = b.getElementsByTagName(c)[0];
+  a.parentNode.insertBefore(d, a);
+})();`;
+
+                return `\n<script src="${utagUrl}"></script>\n\n\n<script type="text/javascript">\n${utagData}\n</script>\n\n\n<script type="text/javascript">\n${utagScript}\n</script>`;
+            };
+
+            const prodCode = generateCode('prod');
+            const nonprodCode = generateCode('qa');
+
+            $('#prod-code').text(prodCode);
+            $('#nonprod-code').text(nonprodCode);
+            $('#form-container').hide();
+            $('#results-container').show();
+        });
+
+        $('#copy-prod').click(function () {
+            navigator.clipboard.writeText($('#prod-code').text());
+            alert('Production code copied to clipboard!');
+        });
+
+        $('#copy-nonprod').click(function () {
+            navigator.clipboard.writeText($('#nonprod-code').text());
+            alert('Non-production code copied to clipboard!');
+        });
+
+        $('#start-over').click(function () {
+            $('#results-container').hide();
+            $('#form-container').show();
+            $('#tealium-form')[0].reset();
+        });
+    });
+});
